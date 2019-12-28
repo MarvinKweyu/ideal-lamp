39a2750c2e8ecbb1d4d10948ab8fc369a10210d3
MarvinKweyu <mkweyu1@gmail.com>
2019-12-28 15:01:04 +0300

    Add form elements and fake search results to index.html

--- jmad/tests.py
+++ jmad/tests.py
@@ -22,29 +22,52 @@ class StudentTestCase(LiveServerTestCase):
         home_page = self.browser.get(self.live_server_url + '/')
 
         """
-        He knows he's in the right place because he can see the name of the site in the heading
+        He knows he's in the right place because he can see the
+        name of the site in the heading
         """
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
-        self.fail("Incomplete Test")
 
         """
         He sees the inputs of the search form, including
         labels and placeholders
         """
+        instrument_input = self.browser.find_element_by_css_selector('input#jmad-instrument')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-instrument"]'))        
+        self.assertEqual(instrument_input.get_attribute('placeholder'),'i.e. trumpet')
+
+
+        artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector('label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute('placeholder'),'i.e. Davis')
 
 
         """
         He types in the name of his instrument and submits it
         """
-        # he sees too many search results...
-
+        instrument_input.send_keys('saxophone')
+        # find button inside form
+        self.browser.find_element_by_css_selector('form button').click()
 
         """
+        He sees too many search results so he adds a part
         So he adds an artist to his search query and gets a more manageable list 
         """
+        # notice plural in elements
+        search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
+        self.assertGreater(len(search_results), 2) # assert that he sees too many results
+        
+        second_artist_input = self.browser.find_element_by_css_selector('input#jmad-artist')
+        second_artist_input.send_keys('Cannonball Adderkey')
+        self.browser.find_element_by_css_selector('form button').click()
 
-        # he clicks the seaarch result
+        second_search_results = self.browser.find_elements_by_css_selector('.jmad-search-result')
+        self.assertEqual(len(second_search_results), 2)
+
+
+        self.fail("Incomplete Test")
+
+        # he clicks the search result
 
         """
         The solo page has the title, artist and album for this particular solo 
